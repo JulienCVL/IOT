08d3755bade1f941784643f0c7e445c2aac54e48
LeKEKW <82328316+LeKEKW@users.noreply.github.com>
2023-11-14 17:43:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Gestion des stages PFE - ENSA Kénitra</title>
+  <title>DataSense - Temperature en Temps Réel</title>
   <meta content="Ce site web favorise l’établissement d’une relation optimale entre la personne étudiante et sa direction de recherche.
            Il contient des informations et des ressources essentielles pour un parcours d’études efficace et simplifié. 
 
@@ -43,7 +43,7 @@ Ce site est disponible pour les étudiants dans la section Étudier en recherche
 
   <!-- Favicons -->
   <!-- <link href="assets/img/logo.png" rel="icon"> -->
-  <link href="https://ensa.uit.ac.ma/wp-content/uploads/2019/11/cropped-ensak-logo.png" rel="icon">
+  <link href="assets/img/logo11.png" rel="icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
@@ -92,8 +92,8 @@ Ce site est disponible pour les étudiants dans la section Étudier en recherche
 
       <div class="carousel-item active">
         <div class="carousel-container">
-          <h2 class="animate__animated animate__fadeInDown">Gestion des stages PFE <span>ENSA Kénitra</span></h2>
-          <p class="animate__animated animate__fadeInUp">Pour que les pratiques d'encadrement suivent une certaine culture propre à L’ENSAK  et qu'ainsi chaque étudiant, chaque professeur puisse s'appuyer sur une formule éprouvée, nous avons conçu un guide qui recense les meilleures pratiques en matière d’encadrement aux études supérieures en recherche.</p>
+          <h2 class="animate__animated animate__fadeInDown">Température Actuelle <br> <span>Mesure de température en Temps Réel</span></h2>
+          <p class="animate__animated animate__fadeInUp">Obtenez la température actuelle en un clin d'œil grâce à notre site web. Soyez informé(e) instantanément des conditions de la température. Notre plateforme offre un accès rapide et simple aux données de température en temps réel.</p>
           <a href="login.php" class="btn-get-started login animate__animated animate__fadeInUp">Se connecter</a>
         </div>
       </div>
@@ -109,11 +109,7 @@ Ce site est disponible pour les étudiants dans la section Étudier en recherche
 
         <div class="section-title">
           <h2 id="about">About</h2>
-          <p>Ce site web favorise l’établissement d’une relation optimale entre la personne étudiante et sa direction de recherche.
-           Il contient des informations et des ressources essentielles pour un parcours d’études efficace et simplifié. 
-
-Pour faire en sorte que l'un comme l'autre connaissent les rôles attendus autant des personnes étudiantes que des directions de recherche, un guide  destiné aux étudiants a également été rédigé. 
-Ce site est disponible pour les étudiants dans la section Étudier en recherche.</p>
+          <p>Chez DataSense Innovation, nous sommes des experts de la gestion thermique. Grâce à nos automates intelligents de mesure de température, nous garantissons un climat optimal. Nos systèmes réagissent rapidement en allumant le chauffage ou le ventilateur lorsque la température dépasse vos limites. Que ce soit pour votre domicile, votre entreprise ou votre installation industrielle, nous vous offrons un confort sans compromis et des économies d'énergie. Choisissez DataSense Innovation pour un contrôle précis de la température, parce que chaque degré compte !</p>
         </div>
 
         <div class="row" data-aos="fade-up">
@@ -121,15 +117,14 @@ Ce site est disponible pour les étudiants dans la section Étudier en recherche
             <img src="assets/img/features-1.svg" class="img-fluid" alt="">
           </div>
           <div class="col-md-7 pt-4">
-            <h3>Stage de fin d’études et premier emploi.</h3>
+            <h3>Mesure de température en Temps Réel.</h3>
             <p class="fst-italic">
-              Le lien entre votre stage de fin d’études et votre premier emploi doit être tangible, c’est pourquoi nous vous conseillons de réfléchir attentivement à plusieurs facteurs avant de faire votre choix.
-
+            Explorez notre gamme de solutions de gestion thermique, découvrez notre engagement envers l'innovation et la qualité, et laissez-nous vous aider à créer un environnement confortable et éco-énergétique. Faites confiance à DataSense Innovation pour la maîtrise de la température, car chaque degré compte !
             </p>
             <ul>
-              <li><i class="bi bi-check"></i> Il est important de choisir votre stage de fin d’études dans une optique de premier emploi.</li>
-              <li><i class="bi bi-check"></i>Prenez donc bien le temps de définir votre projet professionnel, et décidez en conséquence.</li>
-              <li><i class="bi bi-check"></i> N’oubliez pas que ce qui importe, ce sont vos compétences..</li>
+              <li><i class="bi bi-check"></i> Économies d'Énergie : Notre système intelligent réduit les coûts de chauffage/refroidissement inutiles.</li>
+              <li><i class="bi bi-check"></i>Fiabilité : Notre technologie assure un climat stable en toutes circonstances.</li>
+              <li><i class="bi bi-check"></i>Simplicité d'Utilisation : Facile à configurer et à utiliser, même pour les non-techniciens.</li>
             </ul>
           </div>
         </div>
@@ -139,47 +134,16 @@ Ce site est disponible pour les étudiants dans la section Étudier en recherche
             <img src="assets/img/features-2.svg" class="img-fluid" alt="">
           </div>
           <div class="col-md-7 pt-5 order-2 order-md-1">
-            <h3>Statistiques</h3>
+            <h3>Nos points forts</h3>
             <p class="fst-italic">
-             Cette fonctionnalité permet à l'administration de visualiser: 
+             Cette fonctionnalité permet de visualiser: 
             </p>
             <ul>
-              <li><i class="bi bi-check"></i> le pourcentage des étudiants avec et sans encadrant. </li>
-              <li><i class="bi bi-check"></i>Le pourcentage des étudiants inscrits dans le site. </li>
-              <li><i class="bi bi-check"></i>Le pourcentage des stages validés pour la soutenance.</li>
+              <li><i class="bi bi-check"></i> Graphiques de Température : Affichez des graphiques montrant l'évolution de la température dans un espace donné au fil du temps. </li>
+              <li><i class="bi bi-check"></i> Acces à l'historique des mesures de température.</li>
+              <li><i class="bi bi-check"></i> Acces aux mesure de température en temps réel.</li>
+              <li><i class="bi bi-check"></i> Acces aux données brutes.</li>
             </ul>
-          </div>
-        </div>
-
-        <div class="row" data-aos="fade-up">
-          <div class="col-md-5">
-            <img src="assets/img/features-3.svg" class="img-fluid" alt="">
-          </div>
-          <div class="col-md-7 pt-5">
-            <h3>Où faire votre stage de fin d’études</h3>
-            <p>Une des questions que vous devez également vous poser lors de votre choix entre plusieurs PFE, c’est le type de structure que vous souhaitez intégrer.</p>
-            <ul>
-              <li><i class="bi bi-check"></i> Si vous pensez être davantage à l’aise dans une petite structure, préférez postuler et accepter une offre dans ce type d’entreprise plutôt que dans une grande société. Vous aurez généralement moins l’opportunité dans ces dernières de participer à un projet dans sa globalité et aurez moins d’interactions avec les décideurs ; c’est pourquoi il est important de savoir ce que vous recherchez.</li>
-              <li><i class="bi bi-check"></i> Ecoutez votre instinct, si une entreprise vous fait une mauvaise impression, même si les indemnités sont plus élevées, restez vigilants au risque de vous retrouver dans un stage qui ne vous correspond pas. De la même manière, apprenez à discerner les stages qui ne sont « pas sérieux » en demandant l’avis de votre entourage ou de vos professeurs.</li>
-              <li><i class="bi bi-check"></i>Analysez bien les missions que l’on vous propose afin d’éviter de vous ennuyer rapidement ou d’effectuer des tâches qui ne vous permettront pas de monter en compétences.</li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="row" data-aos="fade-up">
-          <div class="col-md-5 order-1 order-md-2">
-            <img src="assets/img/features-4.svg" class="img-fluid" alt="">
-          </div>
-          <div class="col-md-7 pt-5 order-2 order-md-1">
-            <h3>Après votre stage</h3>
-            <p class="fst-italic">
-              A l’issue de votre stage veillez dans la mesure du possible à garder le contact et à rester en bons termes avec vos tuteurs/collègues.
-            </p>
-            <p>
-               N’oubliez pas que votre vie professionnelle ne fait que commencer à la sortie de votre école d’ingénieur ou de l’université ! Remplir son carnet d’adresses dès son stage peut s’avérer très utile. Ceci d’autant plus que vous serez peut-être amené un jour à renseigner des références pour un poste, si votre stage s’est bien déroulé vous pourrez tout à fait demander à votre ancien tuteur de rédiger pour vous une lettre de recommandation.<br>
-
-                Enfin, n’oubliez pas de transmettre une copie de votre rapport de stage à votre tuteur.
-            </p>
           </div>
         </div>
 
@@ -205,21 +169,21 @@ Ce site est disponible pour les étudiants dans la section Étudier en recherche
               <div class="info-box">
                 <i class="bx bx-map"></i>
                 <h3>Our Address</h3>
-                <p>Campus universitaire, B.P 241, Kénitra - Maroc</p>
+                <p>3 Rue de la Chocolaterie, 41000 Blois - France</p>
               </div>
             </div>
             <div class="col-md-6">
               <div class="info-box">
                 <i class="bx bx-envelope"></i>
                 <h3>Email Us</h3>
-                <p>info@example.com<br>contact@example.com</p>
+                <p>info@insa-cvl.fr<br>contact@insa-cvl.fr</p>
               </div>
             </div>
             <div class="col-md-6">
               <div class="info-box">
                 <i class="bx bx-phone-call"></i>
                 <h3>Call Us</h3>
-                <p>Tél : (+212) 5 37 32 94 48<br>Fax : (+212) 5 37 32 92 4</p>
+                <p>Tél : (+33) 5 37 32 94 48<br>Fax : (+33) 5 37 32 92 4</p>
               </div>
             </div>
           </div>
@@ -274,32 +238,22 @@ Ce site est disponible pour les étudiants dans la section Étudier en recherche
             </ul>
           </div>
 
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Services numériques</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="http://ent.uit.ac.ma/">ENT</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="http://ead.uit.ac.ma/moodle/">Moodle</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://www.cnrst.ma/index.php/fr/">Recherche</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://ensa.uit.ac.ma/">ENSAK</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="https://www.uit.ac.ma/">UIT Kénitra</a></li>
-            </ul>
-          </div>
 
           <div class="col-lg-3 col-md-6 footer-contact">
             <h4>Contact Us</h4>
             <p>
-              Campus universitaire, <br>
-              B.P 241, Kénitra<br>
-              Maroc <br><br>
-              <strong>Phone:</strong> (+212) 5 37 32 94 48<br>
-              <strong>Email:</strong> info@example.com<br>
+            3 Rue de la Chocolaterie, <br>
+            41000 Blois<br>
+              France <br><br>
+              <strong>Phone:</strong> (+33) 5 37 32 94 48<br>
+              <strong>Email:</strong> info@insa-cvl.fr<br>
             </p>
 
           </div>
 
           <div class="col-lg-3 col-md-6 footer-info">
             <h3>About this website</h3>
-            <p align="justify">Pour que les pratiques d'encadrement suivent une certaine culture propre à L’ENSAK et qu'ainsi chaque étudiant, chaque professeur puisse s'appuyer sur une formule éprouvée, nous avons conçu un guide qui recense les meilleures pratiques en matière d’encadrement aux études supérieures en recherche.</p>
+            <p align="justify">Explorez notre gamme de solutions de gestion thermique, découvrez notre engagement envers l'innovation et la qualité, et laissez-nous vous aider à créer un environnement confortable et éco-énergétique. Faites confiance à DataSense Innovation pour la maîtrise de la température, car chaque degré compte !</p>
             <div class="social-links mt-3">
               <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
               <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
